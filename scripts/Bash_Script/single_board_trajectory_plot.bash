7af8daf964808bd128ddd067cafceec840f6113c
Ted'sLaptopAtLab <112739938+hellowPluto78700@users.noreply.github.com>
2026-08-12 17:23:40 -0400

    change single board trajectory plot boundary

--- scripts/Bash_Script/single_board_trajectory_plot.bash
+++ scripts/Bash_Script/single_board_trajectory_plot.bash
@@ -2,8 +2,8 @@
 
 python scripts/plot_board_trajectory_window.py \
   --data-root data \
-  --user user_0 \
-  --recording 2 \
+  --user user_7 \
+  --recording 0 \
   --start-s 7 \
   --end-s 9 \
-  --output outputs/plotting_verification/user0_record2_7_9s.png
+  --output outputs/plotting_verification/user7_record0_7_9s.png
