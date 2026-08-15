b5d50782222656ebdc4ccf627b214496b84cb8d6
Zhaolong's PC <112739938+hellowPluto78700@users.noreply.github.com>
2026-08-15 02:41:30 -0400

    Run Experiments A-D with combined Action 0 and Action 1 datasets

--- scripts/bash_script/Encoder_Evaluation_related/reconstruct_spike_sequence.bash
+++ scripts/bash_script/Encoder_Evaluation_related/reconstruct_spike_sequence.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 python scripts/reconstruct_padded_spike_accel.py \
-  outputs/action0_rectified/low-pass/aligned-board-events \
+  outputs/action1_rectified/low-pass/aligned-board-events \
   --overwrite
