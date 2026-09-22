844c0a248116bf3e2a32e62bfc654f1f38b67622
hellopluto787 <112739938+hellowPluto78700@users.noreply.github.com>
2026-09-22 14:26:40 -0400

    Use UTC maintenance deadline for Exp12.0

--- scripts/bash_script/SNN_Bash/run_exp_12_0_dense_cpu_array.bash
+++ scripts/bash_script/SNN_Bash/run_exp_12_0_dense_cpu_array.bash
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=14G
 #SBATCH --time=12:00:00
-#SBATCH --deadline=2026-09-23T05:00:00
+#SBATCH --deadline=2026-09-23T09:00:00
 #SBATCH --output=exp12_0_dense_%A_%a.out
 #SBATCH --error=exp12_0_dense_%A_%a.err
 
